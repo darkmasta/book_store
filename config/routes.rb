74891527b95d383789b4b44eca68194abf7e2748
darkmasta <alperen_bagci@hotmail.com>
2013-11-04 02:44:04 +0200

    Switch authentication system to devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 BookStore::Application.routes.draw do
-  resources :users
   root 'static_pages#home'
 
-  match '/signup',  to: 'users#new',            via: 'get'
+
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
@@ -53,4 +52,4 @@ BookStore::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+end     
